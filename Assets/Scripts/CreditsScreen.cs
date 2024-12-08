70ef4e4f59bf9468f85e4fa5ca3ed87724bf0db1
metro7 <81774994+metro7@users.noreply.github.com>
2024-12-08 18:19:50 -0500

    created credits screen

--- Assets/Scripts/CreditsScreen.cs
+++ Assets/Scripts/CreditsScreen.cs
@@ -3,15 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class WinScreen : MonoBehaviour
+public class CreditsScreen : MonoBehaviour
 {
     public void MainMenu()
     {
         SceneManager.LoadSceneAsync(0);
     }
 
-    public void Credits()
-    {
-        SceneManager.LoadScene(6);
-    }
 }
